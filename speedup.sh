9af8d63159fbc00f48ff6cdaff709ca919da8522
Christer Blomqvist <christer.blomqvist@gmail.com>
2019-02-28 14:35:21 +0100

    Adapt for Cygwin

--- speedup.sh
+++ speedup.sh
@@ -4,16 +4,32 @@
 # Please set DEFAULT_START_DIR to your preferred default start location.
 # NOTE: The order we do things in this file also affects the speed of the startup and this is the order I have found to be the fastest to execute.
 
+# Git for Windows (MINGW) and Cygwin uses different conventions to find the C: drive
+# In Cygwin you need to start the path with /cygdrive while Git for Windows uses just /
+# before the drive letter in the path.
+if [[ $(uname) == CYGWIN* ]]; then
+        echo "Cygwin detected by speedup.sh"
+        ROOT_PREFIX=/cygdrive
+fi
+
+if [[ $(uname) == MINGW* ]]; then
+        echo "MINGW detected by speedup.sh"
+        ROOT_PREFIX=
+fi
+
 # Fix PATH to exclude locations on /h/ by replacing it with references to /c/.
 export PATH=${PATH//\/h\//\/c\/}
 
 # The directory you want to start Git Bash in unless you have used the right click context menu option "Git Bash here...".
 # The default value is ${HOME} which points to "/h/" which is your network drive and you NEVER want to use that one for Git repos.
-DEFAULT_START_DIR=/c/projects
+DEFAULT_START_DIR=${ROOT_PREFIX}/c/projects
 
 # Make Git Bash start by default in ${DEFAULT_START_DIR} unless we used "Git bash here".
-if [ ${PWD} = "/h/" ] ; then
+if [ ${PWD} = "${ROOT_PREFIX}/h/" ] ; then
 	# Detected default bash start in /h/. Changing to desired start dir.
+	cd ${DEFAULT_START_DIR}
+elif [ ${PWD} = "${HOME}" ] ; then
+	# Detected default bash start in ${HOME} other than "${ROOT_PREFIX}/h/". Changing to desired default start dir.
 	cd ${DEFAULT_START_DIR}
 fi
 
@@ -21,11 +37,11 @@ fi
 # It means that it needs to access the network for every time a git command is run.
 # NEWHOME is where we want to store the settings instead.
 # And yes - the trailing / is consistent with the default setting for HOME. It can probably be omitted but that is untested.
-NEWHOME=/c/Users/${USERNAME}/
-OLDHOME="/h/"
+NEWHOME=${ROOT_PREFIX}/c/Users/${USERNAME}
+OLDHOME="${ROOT_PREFIX}/h"
 
 # Make git use C drive for settings
-export HOME=${NEWHOME}
+export HOME=${NEWHOME}/
 
 # Since this trick also stores your settings on your local hard drive you no longer have a network backup of your settings.
 # We might as well take care of that since the startup is slow anyway.
@@ -35,7 +51,7 @@ if [ -f "${NEWHOME}/.gitconfig" ]; then
 	# Backing up any changes in ${NEWHOME}/.gitconfig to ${OLDHOME}/.gitconfig.bak
 	cp "${NEWHOME}/.gitconfig" "${OLDHOME}/.gitconfig.bak"
 else
-	echo "No local '.gitconfig' file found. Copying initial ${OLDHOME}/.gitconfig to ${NEWHOME}/.gitconfig"
+	echo "No local '${NEWHOME}/.gitconfig' file found. Copying initial '${OLDHOME}/.gitconfig' to '${NEWHOME}/.gitconfig'"
 	cp "${OLDHOME}/.gitconfig" "${NEWHOME}/.gitconfig"
 fi
 
